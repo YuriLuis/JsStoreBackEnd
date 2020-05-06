d940d35e0c302d605f9df7b1cc6545b66b8db95e
@yuri_luis_garciia_pereira <yuri.pereira@hbsis.com.br>
2020-05-06 17:42:58 -0300

    Projeto Criado

---
+++ src/main/java/com/yuri/luis/jsstore/LojaJsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.yuri.luis.jsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaJsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaJsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuri/luis/jsstore/LojaJsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.luis.jsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaJsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
